34a98617961846fd3f1b7491b68495d86f76d57c
RamKashyap2050 <ramkashyap2050@gmail.com>
2024-10-26 07:09:28 -0400

    Changed Scrapping algorithm

--- backend/scrappers/IndeedScrapper.js
+++ backend/scrappers/IndeedScrapper.js
@@ -4,12 +4,14 @@ const puppeteer =
   process.env.NODE_ENV === "production"
     ? require("puppeteer-core")
     : require("puppeteer");
+const puppeteerextra = require("puppeteer-extra");
+
 const isProduction = process.env.NODE_ENV === "production";
 
 // Apply stealth plugin only in production
 if (isProduction) {
   const StealthPlugin = require("puppeteer-extra-plugin-stealth");
-  puppeteer.use(StealthPlugin());
+  puppeteerextra.use(StealthPlugin());
 }
 async function scrapeIndeedJobs(
   searchQuery,
